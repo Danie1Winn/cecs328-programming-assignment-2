7ca228d47f1f317a1cc51fab3953a1ffbe116fa5
Danie1Winn <daniel.winn.biz@gmail.com>
2026-03-06 15:46:25 -0800

    feat(`max_trees_sum`): implement sliding window logic which achieves O(n) efficiency

--- cecs328pa2.py
+++ cecs328pa2.py
@@ -3,10 +3,17 @@ def max_trees_sum(tree_array: list[int], grinder_count: int):
     k = grinder_count # the number of grinders available
 
     # checks if there are less trees than grinders
-    if n < k:
-        return "You cannot send less trees than there are grinders."
+    if n < k: return "You cannot send less trees than there are grinders."
     
-    return 0 # placeholder
+    current_sum = sum(tree_array[:k])   # calculate sum of the first k trees
+    max_sum = current_sum               # initialize max_sum with the sum of the first k trees
+
+    # sliding window, used to find the maximum sum of k contiguous trees
+    for i in range(n - k):
+        current_sum += tree_array[i + k] - tree_array[i] # removes "sliding out" element (left side)
+        if current_sum > max_sum: max_sum = current_sum  # adds "sliding in" element (right side), checks if new sum is greater than current
+
+    return max_sum
 
 if __name__ == "__main__":
     try:
